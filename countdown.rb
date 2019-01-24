85a6b2dff025166ade9c2a751d80167c59f7c4e9
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-24 19:51:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
 
-def countdown
+def countdown(countdown_output)
+  while countdown_output >= 1
+  puts "#{countdown_output} SECOND(S)!"
+  countdown_output -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
+countdown(10)
