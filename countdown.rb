b1eda0c5becde94fdcfbb8d8d7fd0e0bea9df1e5
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-24 19:58:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 
 def countdown(countdown_output)
   while countdown_output >= 1
-  puts "#{countdown_output} SECOND(S)!" sleep(1)
+  puts "#{countdown_output} SECOND(S)!" 
   countdown_output -= 1
   end
   "HAPPY NEW YEAR!"
